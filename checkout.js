bcf07008b351a1033864bad4de078122664e1100
David Chung <davidchung0704@gmail.com>
2024-03-21 22:52:59 +1100

    Initial Commit

---
+++ checkout.js
@@ -0,0 +1,50 @@
+const backToHome = document.querySelector('.left-side');
+
+backToHome.addEventListener('click', function() {
+    window.location.href = "index.html";
+});
+
+//Form Validation Client Side
+
+//First-Last Name Input Box Validation
+const first_name = document.querySelector('.first_name');
+const last_name = document.querySelector('.last_name');
+const phone = document.querySelector('.phone');
+
+const letterOnlyRegex = /^[A-Za-z]+$/;
+const integerOnlyRegex = /^[0-9]+$/;
+// if(letterOnlyRegex.test(first_name.value)) {
+//     first_name.style.borderColor = "green";
+// }
+
+// first_name.addEventListener('input', function() {
+//     if(!letterOnlyRegex.test(first_name.value)) {
+//         first_name.style.borderColor = "red";
+//     } else {
+//         first_name.style.borderColor = "grey";
+//     }
+// });
+
+function lettersOnlyInput(input) {
+    input.addEventListener('input', function() {
+        if(!letterOnlyRegex.test(input.value)) {
+            input.style.borderColor = "red";
+        } else {
+            input.style.borderColor = "grey";
+        }
+    });
+}
+
+function integerOnlyInput(input) {
+    input.addEventListener('input', function() {
+        if(!integerOnlyRegex.test(input.value)) {
+            input.style.borderColor = "red";
+        } else {
+            input.style.borderColor = "grey";
+        }
+    });
+}
+
+lettersOnlyInput(first_name);
+lettersOnlyInput(last_name);
+integerOnlyInput(phone);
---
+++ index.js
@@ -0,0 +1,38 @@
+
+let categoryLabel = document.querySelector('.categoryLabel');
+let cartIcon = document.querySelector('.shoppingCartIcon');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
+let container = document.querySelector('.container');
+let shopping = document.querySelector('.shopping');
+
+let categoryDropdown = document.querySelector('.categoryDropdown');
+
+//Category 
+
+shopping.addEventListener('click', function() {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener('click', function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+});
+
+//Category Selection
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var categoryDropdown = document.getElementById('category');
+
+    categoryDropdown.addEventListener('change', function() {
+        var selectedCategory = categoryDropdown.value;
+        window.location.href = 'index.php?categoryDropdown=' + encodeURIComponent(selectedCategory);
+    });
+});
+
+function submitForm() {
+    var form = document.querySelector('.categoryForm');
+    form.submit();
+}
